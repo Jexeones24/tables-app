4bfcbaf5d4984db5d06fb1f152248f7479a92dfc
Jexeones24 <jexeones@gmail.com>
2017-11-08 04:13:24 -0500

    populated state with policy objects, correctly ordered

--- src/App.js
+++ src/App.js
@@ -1,44 +1,36 @@
 import React, { Component } from 'react'
 import './App.css'
+// this import is gross - fix it
 import { fetchFifteenForQuarterMil, fetchTwentyForQuarterMil, fetchTwentyFiveForQuarterMil, fetchFifteenForHalfMil, fetchTwentyForHalfMil, fetchTwentyFiveForHalfMil, fetchFifteenForMil, fetchTwentyForMil, fetchTwentyFiveForMil } from './lib/policyService'
 import Table from './Table'
 
 class App extends Component {
   state = {
+    // make this more reusable
     termsInYears: ['', 15, 20, 25],
     coverage: {
-      250000: {
-        15: 0,
-        20: 0,
-        25: 0
-      },
-      500000: {
-        15: 0,
-        20: 0,
-        25: 0
-      },
-      1000000: {
-        15: 0,
-        20: 0,
-        25: 0
-      }
+      // 250000: {
+      //   15: 0,
+      //   20: 0,
+      //   25: 0
+      // },
+      // 500000: {
+      //   15: 0,
+      //   20: 0,
+      //   25: 0
+      // },
+      // 1000000: {
+      //   15: 0,
+      //   20: 0,
+      //   25: 0
+      // }
     }
   }
 
   sortByPremium = (arr) => arr.sort((a, b) => a.monthly_premium - b.monthly_premium)
 
-  deepMerge = (amount, term, lowest) => {
-    let value = {[term]:lowest}
-    return Object.assign({}, this.state, {
-      coverage: Object.assign({}, this.state.coverage, {
-        [amount]: Object.assign({}, this.state.coverage[amount], {
-          ...value
-        })
-      })
-    })
-  }
-
   componentDidMount() {
+
     let fetches = [
       fetchFifteenForQuarterMil(),
       fetchTwentyForQuarterMil(),
@@ -50,14 +42,27 @@ class App extends Component {
       fetchTwentyForMil(),
       fetchTwentyFiveForMil()
     ]
+
     Promise.all(fetches)
-      .then(fetches => {
-        console.log('all fetches:', fetches)
-        let sorted = fetches.map(f => this.sortByPremium(f))
-        console.log(sorted)
+      .then(policies => {
+        let sorted = policies.map(f => this.sortByPremium(f))
         let lowest = sorted.map(s => s[0])
-        console.log(lowest)
-      })
+        let quarterMil = lowest.filter(f => f.coverage_amount === 250000)
+        let halfMil = lowest.filter(f => f.coverage_amount === 500000)
+        let mil = lowest.filter(f => f.coverage_amount === 1000000)
+        let newState = Object.assign(
+        {}, this.state, {
+          coverage: Object.assign(
+            {}, this.state.coverage, {
+              250000: Object.assign({}, quarterMil),
+              500000: Object.assign({}, halfMil),
+              1000000: Object.assign({}, mil)
+            })
+          })
+        this.setState({coverage: newState}, () =>
+        {console.log('state:', this.state)}
+      )
+    })
   }
 
   render () {
